168fea27d689b148d3faa645aa725942f16e7be6
novais <mathpn2001@gmail.com>
2024-06-29 19:53:21 -0300

    frame video

--- deep/main.py
+++ deep/main.py
@@ -2,6 +2,7 @@ from utils import (read_video,
                    save_video)
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     input_video_path = "input_videos/tennis_match2_cut.mp4"
@@ -28,6 +29,9 @@ def main():
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     save_video(output_video_frames, "output_videos/output.avi")
 
